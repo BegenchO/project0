8961787b4db62dc257573894f3e55256ce22d684
begenchO <begenchorazgeldiyev@gmail.com>
2021-10-15 17:37:41 -0400

    Set up Scala Project. Ready and tested once.

--- src/main/scala/project0/Main.scala
+++ src/main/scala/project0/Main.scala
@@ -8,21 +8,42 @@ object Main {
 
     def main(args: Array[String]) {
 
+        println("----------------------------------------------------")
+        println("Connecting to MongoDB...")
+
         // Connect to mongoDB server and get collection
         val client: MongoClient = MongoClient()
 
+        println("----------------------------------------------------")
+        println("Connection success! Fetching collection from the database...")
+
         val collection: MongoCollection[Document] = client.getDatabase("project0").getCollection("users")
+
+        println("----------------------------------------------------")
+        println("Collection retrieved successfully! Reading JSON file...")
 
         // Read JSON file
         val stringDocuments = Source.fromFile("C:/data.json").getLines.toList
 
+        println("----------------------------------------------------")
+        println("File read success! Preparing Documents to insert into MongoDB...")
+
         // Convert JSON Strings to mongoDB Document type
         val bsonDocuments = stringDocuments.map(doc => Document(doc))
+
+        println("----------------------------------------------------")
+        println("Documents ready! Inserting into the database...")
 
         // Save documents to local mongoDB 
         collection.insertMany(bsonDocuments).printResults()
 
+        println("----------------------------------------------------")
+        println("Successfully saved documents into the database! Closing connection...")
+
         client.close()
+
+        println("----------------------------------------------------")
+        println("Connection closed. Please exit sbt and start mongo shell.")
 
 
     } // end main
